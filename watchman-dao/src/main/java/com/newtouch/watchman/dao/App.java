8e7240c2c7e7a3814f501d8b44dd3b7d0e810ac4
Wanglijun <lijun.wang1@newtouch.cn>
2015-03-31 22:42:38 +0800

    添加watchman-monitor平台的工程目录结构

---
+++ watchman-dao/src/main/java/com/newtouch/watchman/dao/App.java
@@ -0,0 +1,13 @@
+package com.newtouch.watchman.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
